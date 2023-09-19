24a03a16da70af406b7e17344b5407d71da8cd82
Alan Ho <aho0593@gmail.com>
2023-09-19 05:06:09 +0000

    Array types completed

---
+++ understanding-typescript/using-types/object-type.js
@@ -0,0 +1,6 @@
+const person = {
+  name: 'Alan',
+  age: 30,
+  hasPet: true,
+};
+console.log(person.name);
---
+++ understanding-typescript/using-types/object-type.ts
@@ -0,0 +1,8 @@
+const person = {
+  name: 'Alan',
+  age: 30,
+  hasPet: true,
+  hobbies: ['Basketball', 'Cooking', 'Coding'],
+};
+
+console.log(person.name);
