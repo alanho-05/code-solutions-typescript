f1965d7acb070bf802f05a8e4b94237f86bc6b0e
Alan Ho <aho0593@gmail.com>
2023-09-20 21:33:53 +0000

    Class inheritance

--- understanding-typescript/classes/app.js
+++ understanding-typescript/classes/app.js
@@ -1,10 +1,37 @@
 'use strict';
 class Department {
-  constructor(n) {
+  constructor(id, n) {
+    this.employees = [];
+    this.id = id;
     this.name = n;
   }
+
+  describe() {
+    console.log(`Department (${this.id}): ${this.name}`);
+  }
+
+  addEmployee(employee) {
+    this.employees.push(employee);
+  }
+
+  printAllEmployeeInfo() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
 }
-const accounting = new Department('Accounting');
-const hr = new Department('HR');
+class ITDepartment extends Department {
+  constructor(id, admins) {
+    super(id, 'IT');
+    this.admins = admins;
+  }
+}
+const accounting = new Department('1', 'Accounting');
+const hr = new Department('2', 'HR');
+const it = new ITDepartment('d1', ['Alan']);
+accounting.describe();
+accounting.addEmployee('Alan');
+accounting.addEmployee('Jane');
+accounting.printAllEmployeeInfo();
 console.log(accounting);
 console.log(hr);
+console.log(it);
--- understanding-typescript/classes/app.ts
+++ understanding-typescript/classes/app.ts
@@ -1,13 +1,45 @@
 class Department {
-  name: string;
+  private readonly id: string;
+  private name: string;
+  private employees: string[] = [];
 
-  constructor(n: string) {
+  constructor(id: string, n: string) {
+    this.id = id;
     this.name = n;
+  }
+
+  describe(this: Department) {
+    console.log(`Department (${this.id}): ${this.name}`);
+  }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printAllEmployeeInfo() {
+    console.log(this.employees.length);
+    console.log(this.employees);
   }
 }
 
-const accounting = new Department('Accounting');
-const hr = new Department('HR');
+class ITDepartment extends Department {
+  admins: string[];
+  constructor(id: string, admins: string[]) {
+    super(id, 'IT');
+    this.admins = admins;
+  }
+}
 
+const accounting = new Department('1', 'Accounting');
+const hr = new Department('2', 'HR');
+const it = new ITDepartment('d1', ['Alan']);
+
+accounting.describe();
+
+accounting.addEmployee('Alan');
+accounting.addEmployee('Jane');
+
+accounting.printAllEmployeeInfo();
 console.log(accounting);
 console.log(hr);
+console.log(it);
