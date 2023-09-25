e558f23ed02f8ff60e80101f7afb01ba63d8c043
Alan Ho <aho0593@gmail.com>
2023-09-25 19:07:39 +0000

    Delete list items added

--- understanding-typescript/react-typescript/my-app/src/App.tsx
+++ understanding-typescript/react-typescript/my-app/src/App.tsx
@@ -8,13 +8,22 @@ function App() {
   const [todos, setTodos] = useState<Todo[]>([]);
 
   function todoAddHandler(text: string) {
-    setTodos([{ id: Math.random().toString(), text: text }]);
+    setTodos((prevTodos) => [
+      ...prevTodos,
+      { id: Math.random().toString(), text: text },
+    ]);
+  }
+
+  function todoDeleteHanlder(todoId: string) {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.id !== todoId);
+    });
   }
 
   return (
     <div className="App">
       <NewTodo onAddTodo={todoAddHandler} />
-      <TodoList items={todos} />
+      <TodoList items={todos} onDeleteTodo={todoDeleteHanlder} />
     </div>
   );
 }
--- understanding-typescript/react-typescript/my-app/src/components/TodoList.tsx
+++ understanding-typescript/react-typescript/my-app/src/components/TodoList.tsx
@@ -1,12 +1,18 @@
 type TodoListProps = {
   items: { id: string; text: string }[];
+  onDeleteTodo: (id: string) => void;
 };
 
 export default function TodoList(props: TodoListProps): JSX.Element {
   return (
     <ul>
       {props.items.map((todo) => (
-        <li key={todo.id}>{todo.text}</li>
+        <li key={todo.id}>
+          <span>{todo.text}</span>
+          <button onClick={props.onDeleteTodo.bind(null, todo.id)}>
+            Delete
+          </button>
+        </li>
       ))}
     </ul>
   );
